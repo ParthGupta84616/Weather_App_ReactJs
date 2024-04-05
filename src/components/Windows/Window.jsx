0fd903b620645ff316828cba1464e8b8f280116a
ParthGupta84616 <148619447+ParthGupta84616@users.noreply.github.com>
2024-04-05 14:19:28 +0530

    Fixing

--- src/components/Windows/Window.jsx
+++ src/components/Windows/Window.jsx
@@ -6,25 +6,36 @@ import { Line } from 'react-chartjs-2';
 
 function Window({ window, WeatherReports, data }) {
   const [activeButton, setActiveButton] = useState('daily');
-  const [activeButton1, setActiveButton1] = useState('daily1');
 
   const handleButtonClick = (buttonType) => {
     setActiveButton(buttonType);
   };
-  const handleButtonClick1 = (buttonType) => {
-    setActiveButton1(buttonType);
-  };
-
-  console.log(Chart);
-    const TotalReport = { ...WeatherReports, ...data };
+  const TotalReport = { ...WeatherReports, ...data };
+  let daily = [];
+  let datesWithoutPrefix = [];
+  let temperatureMax = [];
+  let temperatureMin = [];
+  
+  if (window === "Temperature"){
+    if (activeButton === "daily") {
+      daily = [TotalReport.daily.time, TotalReport.daily.temperature_2m_max, TotalReport.daily.temperature_2m_min];
+      datesWithoutPrefix = daily[0].map(date => date.substring(5));
+      temperatureMax = daily[1];
+      temperatureMin = daily[2];
+    }
+    else {
+      daily = [TotalReport.hourly.time, TotalReport.hourly.temperature_2m, TotalReport.hourly.relative_humidity_2m];
+      datesWithoutPrefix = daily[0].map(date => date.substring(5));
+      temperatureMax = daily[1];
+      temperatureMin = daily[2];
+    }
+  }
+    console.log(Chart);
     console.log(TotalReport);
-    const datesWithoutPrefix = TotalReport.daily.time.map(date => date.substring(5));
-    const temperatureMax = TotalReport.daily.temperature_2m_max;
-    const temperatureMin = TotalReport.daily.temperature_2m_min;
+    
     const temperatureDifferences = temperatureMax.map((maxTemp, index) => {
         const minTemp = temperatureMin[index];
         const difference = (maxTemp + minTemp)/2;
-        const variation = maxTemp-minTemp;
         return difference.toFixed(1);
     });
     const temperatureVariation = temperatureMax.map((maxTemp, index) => {
@@ -35,65 +46,67 @@ function Window({ window, WeatherReports, data }) {
     
     const MaxValue = Math.max(...temperatureMax)
     const MinValue = Math.min(...temperatureMin)
-    console.log(temperatureVariation);
     const sum = temperatureVariation.reduce((acc, curr) => acc + parseFloat(curr), 0);
-
-    // Calculate average
     const averageVariation = (sum / temperatureVariation.length).toFixed(1);
-    console.log(MinValue);
+    const diff = temperatureDifferences.reduce((acc, curr) => acc + parseFloat(curr), 0);
+    const difference = (diff / temperatureDifferences.length).toFixed(1)
     
-
-    const chartOptions = {
-      plugins: {
-          tooltip: {
-              callbacks: {
-                  label: function(context) {
-                      let label = context.dataset.label || '';
-                      if (label) {
-                          label += ': ';
-                      }
-                      if (context.parsed.y !== null) {
-                          label += context.parsed.y + '°C'; 
-                      }
-                      return label;
-                  }
-              }
-          }
-      },
-      interaction: {
-          mode: 'index',
-          intersect: false,
-      },
-      animation: {
-          duration: 1000,
-          easing: 'easeInOutQuart',
-      },
-      scales: {
-          x: {
-              type: 'category',
-              labels: datesWithoutPrefix,
-              ticks: {
-                  color: 'rgba(0, 0, 0, 1)',
-                  font: {
-                      weight: 'bold',
-                      size: 9,
-                  },
-              },
-          },
-          y: {
-              type: 'linear', 
-              ticks: {
-                  color: 'rgba(0, 0, 0, 1)',
-                  font: {
-                      weight: 'bold',
-                      size: 9,
-                  },
-              },
-          },
-      }
-  };
-  
-    const chartData = {
+    let chartData = {};
+    let chartOptions = {};
+    let chartData1 = {};
+    
+    if (activeButton === "daily") {
+      chartOptions = {
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        let label = context.dataset.label || '';
+                        if (label) {
+                            label += ': ';
+                        }
+                        if (context.parsed.y !== null) {
+                            label += context.parsed.y + '°C'; 
+                        }
+                        return label;
+                    }
+                }
+            }
+        },
+        interaction: {
+            mode: 'index',
+            intersect: false,
+        },
+        animation: {
+            duration: 1000,
+            easing: 'easeInOutQuart',
+        },
+        scales: {
+            x: {
+                type: 'category',
+                labels: datesWithoutPrefix,
+                ticks: {
+                    color: 'rgba(0, 0, 0, 1)',
+                    font: {
+                        weight: 'bold',
+                        size: 9,
+                    },
+                },
+            },
+            y: {
+                type: 'linear', 
+                ticks: {
+                    color: 'rgba(0, 0, 0, 1)',
+                    font: {
+                        weight: 'bold',
+                        size: 9,
+                    },
+                },
+            },
+        }
+      };
+    
+      chartData = {
         labels: datesWithoutPrefix,
         datasets: [
           {
@@ -111,32 +124,36 @@ function Window({ window, WeatherReports, data }) {
             backgroundColor: 'rgba(255, 0, 0, 1)',
             borderColor: 'rgba(255, 0, 0, 1)',
             borderWidth: 1,
-        }
-      ]
-      
-    } 
-    const chartData1 = {
-      labels: datesWithoutPrefix,
-      datasets: [
-        {
-            label: 'Minimum Temperature',
-            data: temperatureMin,
-            fill: '-1', 
-            backgroundColor: 'rgba(0, 0 , 0 , 0)', 
-            borderColor: 'rgba(0, 0, 0, 1)',
-            borderWidth: 2,
-        },
-        {
-          label: 'Difference',
-          data: temperatureDifferences, 
-          fill: false,
-          backgroundColor: 'rgba(255, 0, 0, 1)',
-          borderColor: 'rgba(255, 0, 0, 1)',
-          borderWidth: 2,
-      }
-    ]
+          }
+        ]
+      };
     
-    }           
+      chartData1 = {
+        labels: datesWithoutPrefix,
+        datasets: [
+          {
+              label: 'Minimum Temperature',
+              data: temperatureMin,
+              fill: '-1', 
+              backgroundColor: 'rgba(0, 0 , 0 , 0)', 
+              borderColor: 'rgba(0, 0, 0, 1)',
+              borderWidth: 2,
+          },
+          {
+            label: 'Difference',
+            data: temperatureDifferences, 
+            fill: false,
+            backgroundColor: 'rgba(255, 0, 0, 1)',
+            borderColor: 'rgba(255, 0, 0, 1)',
+            borderWidth: 2,
+          }
+        ]
+      };
+    }
+    else{
+
+    }
+    
 
     return (
       <>
@@ -181,8 +198,8 @@ function Window({ window, WeatherReports, data }) {
       </div>
     </div>
     <div className="w-1/5 h-2/3 mt-auto mb-auto flex flex-col justify-between ml-10">
-      <div className="h-1/2">
-      <div className="flex justify-center -mt-4">
+      <div className="h-full">
+      <div className="flex justify-center -mt-4 ">
         <button
           className={`border-black border-2 p-4 rounded-xl ${activeButton === 'daily' ? 'bg-slate-800' : 'bg-slate-600'}`}
           onClick={() => handleButtonClick('daily')}
@@ -199,26 +216,12 @@ function Window({ window, WeatherReports, data }) {
         </button>
       </div>
       </div>
-
-      <div className="h-1/2">
-      <div className="flex justify-center mt-12">
-        <button
-          className={`border-black border-2 p-4 rounded-xl ${activeButton === 'daily' ? 'bg-slate-800' : 'bg-slate-600'}`}
-          onClick={() => handleButtonClick('daily')}
-        >
-          Daily
-        </button>
+      <div className="ml-10 text-3xl mt-4">
+        Avg Temp 
       </div>
-      <div className="flex justify-center mt-8">
-        <button
-          className={`border-black border-2 p-4 rounded-xl ${activeButton === 'hourly' ? 'bg-slate-800' : 'bg-slate-600'}`}
-          onClick={() => handleButtonClick('hourly')}
-        >
-          Hourly
-        </button>
+      <div className="ml-8 text-3xl p-2 ">
+         {difference}<span>&deg;C</span>
       </div>
-      </div>
-      
       
     </div>
     
